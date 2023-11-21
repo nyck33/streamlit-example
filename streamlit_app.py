7807c541087a1f46ca4309492162825975bb70d4
Nobutaka Kim <nobutaka.gold3@gmail.com>
2023-11-21 23:00:13 +0900

    changed main

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,49 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from langchain import OpenAI, SQLDatabase
+from langchain.chains import create_sql_query_chain
+from snowflake.snowpark import Session
+import os
 
-"""
-# Welcome to Streamlit!
+# Streamlit app structure
+def main():
+    # Load secrets for Snowflake and OpenAI
+    snowflake_url = st.secrets["snowflake_url"]
+    OpenAI_API_KEY = st.secrets["OpenAI_API_KEY"]
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    # Streamlit UI components
+    st.title("Natural Language SQL Query Generator")
+    natural_language_query = st.text_area("Enter your query in natural language:")
+    submit_button = st.button("Generate SQL Query")
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    # Connection to the database
+    db = SQLDatabase.from_uri(snowflake_url, sample_rows_in_table_info=1, include_tables=['orders', 'locations'])
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+    if submit_button and natural_language_query:
+        llm = OpenAI(temperature=0, openai_api_key=OpenAI_API_KEY)
+        # Process the natural language query and generate SQL
+        database_chain = create_sql_query_chain(llm, db)
+        generated_sql_query = database_chain(natural_language_query)
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+        # Display the generated SQL query
+        st.text("Generated SQL Query:")
+        st.code(generated_sql_query)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+        # Execute and display query results
+        session = Session.builder.configs(db.session_configs).create()
+        query_result = session.sql(generated_sql_query).collect()
+        formatted_results = format_query_results(query_result)
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+        # Display results in a scrollable box
+        st.text("Query Results:")
+        st.text_area("", value=formatted_results, height=300)
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+def format_query_results(query_result):
+    # Format the Snowflake query results for display
+    # Implement the formatting logic as per your requirements
+    # This is a placeholder - update it to match your actual formatting needs
+    formatted_result = "\n".join([str(row) for row in query_result])
+    return formatted_result
+
+# Run the app
+if __name__ == "__main__":
+    main()
